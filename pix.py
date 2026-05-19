72fcfc4bf43a0a7342e4bfcc6cf34b217753050b
Nadine <nadinecassiacosta@gmail.com>
2026-05-19 10:38:42 -0300

    Criando a classe Pix. Ela herda da classe Pagamento

---
+++ pix.py
@@ -0,0 +1,16 @@
+from datetime import date
+from pagamento import Pagamento
+
+
+class Pix(Pagamento):
+    def __init__(self, data: date, valor_pago: float, cpf_pagador: str) -> None:
+        super().__init__(data, valor_pago)
+        self.__cpf_pagador = cpf_pagador
+
+    @property
+    def cpf_pagador(self):
+        return self.__cpf_pagador
+    
+    @cpf_pagador.setter
+    def cpf_pagador(self, cpf_pagador):
+        self.__cpf_pagador = cpf_pagador
