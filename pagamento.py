cd422efcb5516f6eb3ed1955690ea0a6e5192851
Nadine <nadinecassiacosta@gmail.com>
2026-06-02 10:58:10 -0300

    processamento do pagamento

--- pagamento.py
+++ pagamento.py
@@ -11,15 +11,19 @@ class Pagamento(ABC):
     @property
     def data(self):
         return self.__data
-    
+
     @property
     def valor_pago(self):
         return self.__valor_pago
-    
+
     @data.setter
     def data(self, data):
         self.__data = data
-    
+
     @valor_pago.setter
     def valor_pago(self, valor_pago):
         self.__valor_pago = valor_pago
+
+    @abstractmethod
+    def processar_pagamento(self) -> None:
+        pass
