640cfbc43f8ca378f9a12a8b82f072b82a16c661
Nadine <nadinecassiacosta@gmail.com>
2026-06-02 10:42:38 -0300

    retiro do pass

--- dinheiro.py
+++ dinheiro.py
@@ -5,5 +5,3 @@ from pagamento import Pagamento
 class Dinheiro(Pagamento):
     def __init__(self, data: date, valor_pago: float) -> None:
         super().__init__(data, valor_pago)
-        pass
-    
