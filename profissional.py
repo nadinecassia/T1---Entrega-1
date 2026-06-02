4a6111e2f3d312340f8c9cb64a591d3cbbd16b7f
Nadine <nadinecassiacosta@gmail.com>
2026-06-02 10:05:40 -0300

    formatação

--- profissional.py
+++ profissional.py
@@ -2,23 +2,30 @@ from pessoa import Pessoa
 
 
 class Profissional(Pessoa):
-    def __init__(self, nome: str, cpf: str, celular: str, especialidade: str, registro_profissional: str) -> None:
+    def __init__(
+        self,
+        nome: str,
+        cpf: str,
+        celular: str,
+        especialidade: str,
+        registro_profissional: str,
+    ) -> None:
         super().__init__(nome, cpf, celular)
         self.__especialidade = especialidade
         self.__registro_profissional = registro_profissional
-    
+
     @property
     def especialidade(self):
         return self.__especialidade
-    
+
     @property
     def registro_profissional(self):
         return self.__registro_profissional
-    
+
     @especialidade.setter
-    def especialidade(self, especialidade):
+    def especialidade(self, especialidade) -> None:
         self.__especialidade = especialidade
 
     @registro_profissional.setter
-    def registro_profissional(self, registro_profissional):
+    def registro_profissional(self, registro_profissional) -> None:
         self.__registro_profissional = registro_profissional
